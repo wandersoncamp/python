237781e6f67c1af3d60c7b024475430b14e02808
Wanderson Pacheco <wandersoncamp1308@gmail.com>
2018-07-24 11:56:52 -0300

    Add files via upload

---
+++ vendedor.py
@@ -0,0 +1,8 @@
+nome=input("Informe seu nome: ")
+salario=float(input("Informe o valor do seu salario: "))
+vendas=float(input("Informe o valor total de vendas: "))
+
+print("Vendedor: ", nome)
+print("Salário fixo: ", salario)
+print("Salário total: ", salario + vendas * 0.15)
+  
